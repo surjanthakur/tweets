3c413308c220468f12d7a00b1c8289c25b31fd4f
surjanthakur <tsurjan506@gmail.com>
2026-01-24 09:30:41 +0530

    save

--- README.md
+++
@@ -1,2 +0,0 @@
-# tweets
-this repo i build for developer to  share their coding problems share with other devs.
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,2 @@
 def main():
-    print("Hello from backend!")
-
-
-if __name__ == "__main__":
-    main()
+    pass
